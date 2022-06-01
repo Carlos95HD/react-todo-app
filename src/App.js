727dae20d940d1455e37a5e8d346c61671305665
Carlos <hernandeasi7@gmail.com>
2022-06-01 02:19:55 -0300

    Updated | Deleted unnecessary styles and component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styled-components/GlobalStyles/Themes";
 import { useDarkMode } from "./hooks/useDarkMode";
 import { GlobalStyles } from "./styled-components/GlobalStyles/GlobalStyles";
-import styled from "styled-components";
 import Toggle from "./styled-components/Layout/Toggler";
-import { Title } from "./styled-components/Layout/TItle";
-
-const Header = styled.div`
-  background-image: ${({ theme }) => theme.background};
-  background-size: cover;
-  height: 100%;
-  text-align: center;
-  width: 100%;
-  transition: background-image 0.5s linear;
-`;
+import { TasksList } from "./components/TasksList";
+import { AddTasks } from "./components/AddTasks";
+import { Header } from "./styled-components/Layout/Header";
+import { CountriesProvider } from "./todo/TodoContext";
 
 const App = () => {
   const [theme, themeToggler, mountedComponent] = useDarkMode();
   if (!mountedComponent) return <div />;
 
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <>
-        <GlobalStyles />
-        <Header>
-          <Title />
-          <Toggle theme={theme} toggleTheme={themeToggler} />
-        </Header>
-      </>
-    </ThemeProvider>
+    <CountriesProvider>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+  
+          <GlobalStyles />
+          <Header>
+            <div>
+              <h1>TODO</h1>
+              <Toggle theme={theme} toggleTheme={themeToggler} />
+            </div>
+            {/* input to add todo */}
+            <AddTasks />
+          </Header>
+          {/* TasksList */}
+          <TasksList />
+
+      </ThemeProvider>
+    </CountriesProvider>
   );
 };
 export default App;
